f3dcd946bdc9f7364410023f4ad3efd2a55cc9a7
Someone <93450381+Kaiowa15@users.noreply.github.com>
2022-12-02 11:57:43 -0300

    Add files via upload

--- Move_File.py
+++ Move_File.py
@@ -12,20 +12,20 @@ for file_name in list_of_files:
 
     if extension =="":
         continue
-    if extension in ['.txt,.doc,.docx,.pdf']:
+    if extension in ['.txt','.doc','.docx','.pdf']:
         path1 = from_dir + '/' + file_name
         path2 = to_dir + '/' + "Arquivos_texto"
         path3 = to_dir + '/' + "Arquivos_texto" + '/' + file_name
         print("path1 " , path1)
         print("path3 ", path3)
 
-    if os.path.exists(path2):
-            print("movendo o arquivo", file_name + "[................]")
+        if os.path.exists(path2):
+                print("movendo o arquivo", file_name + "[................]")
 
-            shutil.move(path1, path3)
-    else:
-            os.makedirs(path2)
-            print("movendo o arquivo", file_name + "[................]")
-            shutil.move(path1,path3)
+                shutil.move(path1, path3)
+        else:
+                os.makedirs(path2)
+                print("movendo o arquivo", file_name + "[................]")
+                shutil.move(path1,path3)
 
-#tem alguma coisa dando errado mas não tenho ideia do que seja, e não tem como pegar o cominho documentos
+    #tem alguma coisa dando errado mas não tenho ideia do que seja, e não tem como pegar o cominho documentos
